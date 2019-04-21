500f6e142d0d2202c6a407351e4459a24aa0bc83
KingHenrythe8th <jonathancourt@gmail.com>
2019-04-21 09:41:47 +0000

    Add solution

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -1,4 +1,25 @@
-# Day 1 solution - day1func()
+# Day 1 solution part 1
 
-def day1func(x):
-    return "foo"
+def day1func_part1(text):
+  counter = iter(text)
+  captcha = 0
+  pos_1 = text[(len(text) - 1)]
+
+  while True:
+    try:
+      pos_2 = counter.next()
+      if pos_1 == pos_2:
+        captcha = captcha + int(pos_1)
+        pos_1 = pos_2
+      else:
+        pos_1 = pos_2
+        continue
+    except StopIteration:
+      break
+
+  return captcha
+
+with open('input.txt', 'r') as file:
+    text = file.readline().strip()
+
+print(day1func_part1(text))
--- Day1/Test1.py
+++ Day1/Test1.py
@@ -3,17 +3,19 @@
 # 1122 produces a sum of 3 (1 + 2) because the first digit (1) matches the
 # second digit and the third digit (2) matches the fourth digit.
 
-from Day1 import day1func
+from Day1 import day1func_part1
 
-assert day1func(1122) == 3, ("1122 produces a sum of 3 (1 + 2) because the first \
+assert day1func_part1("1122") == 3, ("1122 produces a sum of 3 (1 + 2) because the first \
  digit (1) matches the second digit and the third digit (2) matches \
  the fourth digit.")
 
-assert day1func(1111) == 4, ("1111 produces 4 because each digit (all 1) \
+assert day1func_part1("1111") == 4, ("1111 produces 4 because each digit (all 1) \
 matches the next.")
 
-assert day1func(1234) == 0, ("1234 produces 0 because no digit matches the \
+assert day1func_part1("1234") == 0, ("1234 produces 0 because no digit matches the \
 next.")
 
-assert day1func(91212129) == 9, ("91212129 produces 9 because the only \
+assert day1func_part1("91212129") == 9, ("91212129 produces 9 because the only \
 digit that matches the next one is the last digit, 9.")
+
+print ("Tests passed")
