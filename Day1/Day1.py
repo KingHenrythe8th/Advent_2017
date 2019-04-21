9b5f0cac0188e37b7a270cc357a4a1c8745cdc32
KingHenrythe8th <jonathancourt@gmail.com>
2019-04-21 08:43:04 +0000

    Test added, Day1 added

---
+++ Day1/Day1.py
@@ -0,0 +1,4 @@
+# Day 1 solution - day1func()
+
+def day1func(x):
+    return "foo"
---
+++ Day1/Test1.py
@@ -0,0 +1,19 @@
+# Use Day1 function to assert 3 statements
+
+# 1122 produces a sum of 3 (1 + 2) because the first digit (1) matches the
+# second digit and the third digit (2) matches the fourth digit.
+
+from Day1 import day1func
+
+assert day1func(1122) == 3, ("1122 produces a sum of 3 (1 + 2) because the first \
+ digit (1) matches the second digit and the third digit (2) matches \
+ the fourth digit.")
+
+assert day1func(1111) == 4, ("1111 produces 4 because each digit (all 1) \
+matches the next.")
+
+assert day1func(1234) == 0, ("1234 produces 0 because no digit matches the \
+next.")
+
+assert day1func(91212129) == 9, ("91212129 produces 9 because the only \
+digit that matches the next one is the last digit, 9.")
