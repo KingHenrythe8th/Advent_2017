4c8c041fc4f50fc6488b2d93de25330b7fdbf07e
KingHenrythe8th <jonathancourt@gmail.com>
2019-04-22 20:29:56 +0000

    Add solution Day 2 part 2

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -7,5 +7,27 @@ def day2func_part1(x):
       checksum = checksum + max(num_T) - min(num_T)
     return checksum
 
+
+
+def day2func_part2(x):
+    checksum = 0
+    for num in x.readlines():
+      L = num.split('\t')
+      num_L = []
+      num_T = sorted([int(l.strip()) for l in L], reverse=True)
+      for n in num_T:
+        for q in num_T:
+          i_n = num_T.index(n)
+          i_q = num_T.index(q)
+          nq = float(n)/q
+          qn = float(q)/n
+          if nq.is_integer() and i_n != i_q:
+            checksum = checksum + nq
+          elif qn.is_integer() and i_n != i_q:
+            checksum = checksum + qn
+          else:
+            continue
+    return checksum / 2
+
 with open('input.txt', 'r') as f:
-    print(day2func_part1(f))
+    print(day2func_part2(f))
---
+++ Day2/README_PART2.md
@@ -0,0 +1,6 @@
+--- Part Two ---
+"Great work; looks like we're on the right track after all. Here's a star for your effort." However, the program seems a little worried. Can programs be worried?
+
+"Based on what we're seeing, it looks like all the User wanted is some information about the evenly divisible values in the spreadsheet. Unfortunately, none of us are equipped for that kind of calculation - most of us specialize in bitwise operations."
+
+It sounds like the goal is to find the only two numbers in each row where one evenly divides the other - that is, where the result of the division operation is a whole number. They would like you to find those numbers on each line, divide them, and add up each line's result.
